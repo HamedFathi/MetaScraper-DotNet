89c10b4404af5282d193744f947e540bae84ac05
Hussein Habibi Juybari <Hussein.Juybari@gmail.com>
2019-03-07 15:09:11 +0330

    Refactor get image and title of url
    Error handling

--- scraper-DotNet/Controllers/MetaScraperController.cs
+++ scraper-DotNet/Controllers/MetaScraperController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
+using System.Text;
 using System.Threading.Tasks;
 using System.Xml;
 using HtmlAgilityPack;
@@ -25,6 +26,11 @@ namespace scraperDotNet.Controllers
         [HttpGet("{url}")]
         public async Task<IActionResult> GetUrlMetaData(String url)
         {
+            if (string.IsNullOrEmpty(url))
+            {
+                return NotFound(new ApiErrorModel(500, "Url parameter should has value."));
+            }
+
             MetaDataModel metaData;
             var httpClient = new HttpClient();
             UriBuilder uriBuild = new UriBuilder(url);
@@ -39,7 +45,7 @@ namespace scraperDotNet.Controllers
             }
 
             var siteString = await responseData.Content.ReadAsStringAsync();
-            
+
             HtmlDocument document = new HtmlDocument();
             document.LoadHtml(siteString);
             var metaTags = document.DocumentNode.SelectNodes("//meta");
@@ -49,7 +55,10 @@ namespace scraperDotNet.Controllers
 
             if (metaTags != null)
             {
-                foreach(var tag in metaTags)
+                metaData.Title = GetTitleFrom(document);
+                metaData.ImageInfo = GetImageInfoFrom(document);
+
+                foreach (var tag in metaTags)
                 {
                     var tagName = tag.Attributes["name"];
                     var tagContent = tag.Attributes["content"];
@@ -60,10 +69,6 @@ namespace scraperDotNet.Controllers
                     {
                         switch (tagName.Value.ToLower())
                         {
-                            case "title":
-                                metaData.Title = tagContent.Value;
-                                break;
-
                             case "description":
                                 if (!String.IsNullOrEmpty(tagContent.Value))
                                 {
@@ -81,23 +86,10 @@ namespace scraperDotNet.Controllers
                         }
                     }
 
-                    if(tagItemprop != null)
-                    {
-                       if (tagItemprop.Value.Contains("image"))
-                        {
-                            metaData.ImageInfo = new Models.ImageInfoModel(tagContent.Value);
-                        }
-                    }
-
                     if (tagProperty != null)
                     {
                         switch (tagProperty.Value.ToLower())
                         {
-                            case "twitter:title":
-                            case "og:title":
-                                metaData.Title = string.IsNullOrEmpty(metaData.Title) ? tagContent.Value : metaData.Title;
-                                break;
-
                             case "twitter:description":
                             case "og:descriptionn":
                                 metaData.Descriptions = string.IsNullOrEmpty(metaData.Descriptions) ? tagContent.Value : metaData.Descriptions;
@@ -106,19 +98,6 @@ namespace scraperDotNet.Controllers
                             case "og:locale":
                                 metaData.Locale = tagContent.Value;
                                 break;
-
-                            case "twitter:image":
-                            case "og:image":
-                                if (metaData.ImageInfo == null)
-                                {
-                                    metaData.ImageInfo = new Models.ImageInfoModel(tagContent.Value);
-                                }
-                                else
-                                {
-                                    metaData.ImageInfo.ImgUrl = tagContent.Value;
-                                }
-                                break;
-
 
                             case "og:image:width":
                                 if (metaData.ImageInfo == null)
@@ -152,52 +131,111 @@ namespace scraperDotNet.Controllers
                 }
             }
 
-            // If there isn't any meta tag for image, try to get fav icon of website page.
-            if ((metaData.ImageInfo == null || String.IsNullOrEmpty(metaData.ImageInfo.ImgUrl)))
-            {
-                var otherTags = document.DocumentNode.SelectNodes("//link");
-
-                if (otherTags != null)
-                {
-                    var favIcon = otherTags.First((tag) => tag.Attributes["rel"].Value.ToLower() == "icon");
-
-                    if (favIcon != null)
-                    {
-                        var tagProperty = favIcon.Attributes["href"];
-
-                        if (metaData.ImageInfo == null)
-                        {
-                            metaData.ImageInfo = new Models.ImageInfoModel(tagProperty.Value);
-                        }
-                        else
-                        {
-                            metaData.ImageInfo.ImgUrl = tagProperty.Value;
-                        }
-                    }
-                }
-
-            }
-
-            // If there isn't any meta tag for description, try to get content of website page.
-            if (metaData.Title == null)
-            {
-                var otherTags = document.DocumentNode.SelectNodes("//title");
-
-                if (otherTags != null)
-                {
-                    var title = otherTags[0];
-
-                    if (title != null)
-                    {
-                        metaData.Title = title.InnerHtml;
-                    }
-                }
-
-            }
 
             return Ok(metaData);
         }
 
+        /// <summary>
+        /// Gets the title from html content.
+        /// </summary>
+        /// <returns>The title of website.</returns>
+        /// <param name="document">Document is content of website</param>
+        private string GetTitleFrom(HtmlDocument document)
+        {
+            var metaTags = document.DocumentNode.SelectNodes("//meta");
+
+            var mTitle = metaTags.FirstOrDefault((tag) =>
+            {
+                return tag?.Attributes["name"]?.Value == "title";
+            });
+
+            if (mTitle == null)
+            {
+                mTitle = metaTags.FirstOrDefault((tag) =>
+                {
+                    return tag?.Attributes["proprty"]?.Value == "twitter:title" ||
+                                tag?.Attributes["proprty"]?.Value == "og:title";
+                });
+            }
+
+            if (mTitle != null)
+            {
+                return mTitle.Attributes["content"].Value;
+            }
+
+            // If there isn't any meta tag for description, try to get content of website page.
+            var otherTags = document.DocumentNode.SelectNodes("//title");
+
+            if (otherTags != null)
+            {
+                var title = otherTags[0];
+
+                if (title != null)
+                {
+                    return title.InnerHtml;
+                }
+            }
+            return null;
+        }
+
+        /// <summary>
+        /// Gets the image info from website.
+        /// </summary>
+        /// <returns>The image info of website.</returns>
+        /// <param name="document">Document is content of website</param>
+        private ImageInfoModel GetImageInfoFrom(HtmlDocument document)
+        {
+            ImageInfoModel imageInfo = null;
+            var metaTags = document.DocumentNode.SelectNodes("//meta");
+
+            var mImage = metaTags.FirstOrDefault((tag) =>
+            {
+                return tag?.Attributes["itemprop"]?.Value == "image";
+            });
+
+            if (mImage == null)
+            {
+                mImage = metaTags.FirstOrDefault((tag) =>
+                {
+                    return tag?.Attributes["proprty"]?.Value == "twitter:image" ||
+                                tag?.Attributes["proprty"]?.Value == "og:image";
+                });
+            }
+
+            if (mImage != null && !string.IsNullOrEmpty(mImage.Attributes["content"].Value))
+            {
+                imageInfo = new ImageInfoModel(mImage.Attributes["content"].Value);
+            }
+
+            // If there isn't any meta tag for image, try to get fav icon of website page.
+            var otherTags = document.DocumentNode.SelectNodes("//link");
+            if (otherTags != null)
+            {
+                var favIcon = otherTags.FirstOrDefault((tag) => tag?.Attributes["rel"]?.Value?.ToLower() == "icon");
+
+                if (favIcon != null)
+                {
+                    var tagProperty = favIcon?.Attributes["href"];
+
+                    if (imageInfo == null)
+                    {
+                        imageInfo = new Models.ImageInfoModel(tagProperty?.Value);
+                    }
+                    else
+                    {
+                        imageInfo.ImgUrl = tagProperty.Value;
+                    }
+                }
+            }
+
+            return imageInfo;
+        }
+
+        /// <summary>
+        /// Ises the valid URL.
+        /// </summary>
+        /// <returns><c>true</c>, if valid URL was ised, <c>false</c> otherwise.</returns>
+        /// <param name="url">URL.</param>
         private static Boolean isValidUrl(String url)
         {
             if (string.IsNullOrEmpty(url))
@@ -215,6 +253,5 @@ namespace scraperDotNet.Controllers
                 return false;
             }
         }
-
     }
 }
